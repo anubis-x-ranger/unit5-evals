3d6b789bea57fcf918b6b6201ff49272312feab6
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-04-11 12:10:33 +0530

    update 2

--- eval1/cricket-score-tracker/src/App.jsx
+++ eval1/cricket-score-tracker/src/App.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './app.css';
 function App() {
+    const [runs,setRuns]=React.useState(76);
+    const [wickets,setWickets]=React.useState(2);
+
+    const handleincrease=(inc)=>{
+        setRuns(runs+inc);
+    }
+    const wicketsincrease=(wik)=>{
+        setWickets(wickets+wik);
+    }
     return (
       <div className="App">
         <h3>India:</h3>
         <div className="banner">
           <div>
-            Score:{" "}
+            Score:
             <h1 className="scoreCount">
-              {
-                // show "score" here
-              }
+              {runs}
             </h1>
           </div>
           <div>
             Wicket:{" "}
             <h1 className="wicketCount">
-              {
-                // show wicket here
-              }
+              {wickets}
             </h1>
           </div>
   
@@ -36,15 +42,15 @@ function App() {
         <div className="addScore">
           Add Score
           {/* these buttons should add the respective amount in the score */}
-          <button className="addScore1">Add 1</button>
-          <button className="addScore4">Add 4</button>
-          <button className="addScore6">Add 6</button>
+          <button className="addScore1" onClick={()=>handleincrease(1)}>Add 1</button>
+          <button className="addScore4" onClick={()=>handleincrease(4)}>Add 4</button>
+          <button className="addScore6" onClick={()=>handleincrease(6)}>Add 6</button>
         </div>
   
         <div className="addWicket">
           Add Wicket
           {/* Increase the count of wicket */}
-          <button>Add 1 wicket</button>
+          <button onClick={()=>wicketsincrease(1)}>Add 1 wicket</button>
         </div>
   
         <div className="addBall">
